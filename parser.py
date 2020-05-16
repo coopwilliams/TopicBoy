9a72f444ad7b7feb5b0dbf5158730d6c3ca9dac9
coopwilliams <ken.c.williams.iii@gmail.com>
2020-05-16 01:06:41 -0400

    add comments. No epub text yet

--- parser.py
+++ parser.py
@@ -17,7 +17,35 @@ def read_PDF(filepath):
 
 def read_EPUB(filepath):
     book = epub.read_epub(filepath)
-    pass
+
+    # this package has a get_metadata() function.
+    # But this helper function will get the same result
+    # regardless of the namespace, which can be 'DC' or 'OPF'
+    def find_by_key(data, target):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                yield from find_by_key(value, target)
+            elif key == target:
+                yield value
+
+    # TODO: figure out how to read the text (currently bytestring)
+
+    title = next(find_by_key(book.metadata, 'title'))
+    author = next(find_by_key(book.metadata, 'creator'))
+
+    # get the string values out of lists or tuples
+    while isinstance(title, list) or isinstance(title, tuple):
+        title = title[0] 
+    while isinstance(author, list) or isinstance(author, tuple):
+        author = author[0] 
+
+    doc = {
+        'filepath' : filepath,
+        'full_text' : full_text,
+        'title' : title,
+        'author' : author,
+    }
+    return doc
 
 def read_MOBI(filepath):
     pass
