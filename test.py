cda7f5284c74b78e730e1172d2d5e96cfac7cb06
coopwilliams <ken.c.williams.iii@gmail.com>
2020-05-18 14:40:47 -0400

    fix references in tests

--- test.py
+++ test.py
@@ -11,30 +11,30 @@ class ReadingTest(unittest.TestCase):
     def setUp(self):
         self.test_documents = [f for f in listdir(test_dir) if isfile(join(test_dir, f))]
     
-    def test_EPUB(self):
+    def test_epub(self):
         for i in self.test_documents:
             if i[-4:] == "epub":
-                doc = reader.read_EPUB(test_dir + "/" + i)
+                doc = reader.read_epub(test_dir + "/" + i)
         print(doc['filepath'])
         print(doc['title'])
         print(doc['author'])
         print(doc['full_text'][10:], "\n")
         self.assertGreater(len(doc['full_text']), 0)
     
-    def test_PDF(self):
+    def test_pdf(self):
         for i in self.test_documents:
             if i[-3:] == "pdf":
-                doc = reader.read_PDF(test_dir + "/" + i)
+                doc = reader.read_pdf(test_dir + "/" + i)
         print(doc['filepath'])
         print(doc['title'])
         print(doc['author'])
         print(doc['full_text'][10:], "\n")
         self.assertGreater(len(doc['full_text']), 0)
     
-    def test_MOBI(self):
+    def test_mobi(self):
         for i in self.test_documents:
             if i[-4:] == "mobi":
-                doc = reader.read_MOBI(test_dir + "/" + i)
+                doc = reader.read_mobi(test_dir + "/" + i)
         print(doc['filepath'])
         print(doc['title'])
         print(doc['author'])
