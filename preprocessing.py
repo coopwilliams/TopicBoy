d27d66f47a1d6f1917dabdd8450131333ed87504
coopwilliams <ken.c.williams.iii@gmail.com>
2020-05-18 15:06:02 -0400

    remove non-alphabet chars

--- preprocessing.py
+++ preprocessing.py
@@ -4,4 +4,10 @@ def remove_special(text):
     """Remove digits, special chars like \n, \t"""
 
     no_special = re.sub("\s+|\d+", " ", text)
-    return no_special
+    return " ".join(no_special.split())
+
+def remove_non_alpha(text):
+    """Remove all non-alphabet chars"""
+    all_alpha = re.sub("[^a-zA-Z]+", " ", text)
+    return " ".join(all_alpha.split())
+
